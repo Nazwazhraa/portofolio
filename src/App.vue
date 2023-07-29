43a2ca6a21afa85013286711458bd140d5d3d5a6
Your Name <you@example.com>
2023-07-29 12:27:09 +0700

    Portofolio

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavbarComponents from './components/NavbarComponents.vue';
+import HeaderComponents from './components/HeaderComponents.vue';
+import FooterComponents from './components/FooterComponents.vue';
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<NavbarComponents></NavbarComponents>
+<HeaderComponents></HeaderComponents>
+<FooterComponents></FooterComponents>
+</template>
+
---
+++ src/components/HeaderComponents.vue
@@ -0,0 +1,263 @@
+<template>
+<section class="bg-center bg-no-repeat bg-[url('./aku.PNG')] bg-gray-700 bg-blend-multiply">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Hi.</h1>
+        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">It's me, Nazwa Az Zahra</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+        </div>
+    </div>
+</section>
+<center>
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16" style="background-image: url('putih1.jpg');">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-10"></div>
+    <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="me.jpg" alt="">
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nazwa Az Zahra</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        <table>
+            <tr>
+                <td>TTL </td>
+                <td> :</td>
+                <td>Bandung, 02 Agustus 2006</td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td>:</td>
+                <td>nazwaazzahra028@gmail.com</td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td>Komp. Permata Kopo Blok F </td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td>:</td>
+                <td>Perempuan</td>
+            </tr>
+            <tr>
+                <td>Agama</td>
+                <td>:</td>
+                <td>Islam</td>
+            </tr>
+        </table>
+        </p>
+    </div>
+</a>
+<div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-5"></div>
+<div class="inline-flex items-center justify-center w-full">
+    </div>
+    <div class="absolute px-4 -translate-x-1/2 bg-white left-1/2 dark:bg-gray-900">
+        <svg class="w-4 h-4 text-gray-700 dark:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+    <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+  </svg>
+    </div>
+    </div>
+
+<div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-20">
+<h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">A b o u t </span> Me</h1>
+<p class="text-gray-500 font-bold dark:text-gray-400">Nazwa Az Zahra, Gadis yang kini berusia 16 tahun yang sudah tertarik menjadi penulis sebuah novel. Berhasil mengarang cerita yang dituliskan di 2 judul dan wattpad sekaligus. 2 kali juga menjadikan pemenang hatinya sebagai pemeran utama di ceritanya, i.
+Lahir di Bandung pada tanggal 02 Agustus 2006. 
+Meskipun tidak memiliki latar belakang pendidikan di bidang Sastra, saya selalu tertarik dengan bidang menulis. Hal inilah yang membuat saya memiliki hobi sebagai penulis sebuah cerita dan novel meskipun tidak banyak yang mengetahui hal ini, hehe.
+Sejak duduk di bangku SD, saya sudah sering mengarang dan menulis walaupun hanya bisa menulis di buku tulis menggunakan sebuah pensil.
+Tetapi hal ini membuat saya tidak percaya diri, karena terlalu sering membaca dan menulis terlalu dekat, saya memakai alat bantu untuk melihat, yap  kacamata. </p>
+<div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-5"></div>
+<div class="inline-flex items-center justify-center w-full">
+    </div>
+    <div class="absolute px-4 -translate-x-1/2 bg-white left-1/2 dark:bg-gray-900">
+        <svg class="w-4 h-4 text-gray-700 dark:text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
+    <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
+  </svg>
+    </div>
+    </div>
+    <p class="text-4xl font-extrabold text-gray-900 dark:text-white">Jenjang Pendidikan</p>
+</center>
+<div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-16">
+<ol class="relative border-l border-gray-200 dark:border-gray-700">                  
+    <li class="mb-10 ml-6">            
+        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </span>
+        
+        <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">SEKOLAH DASAR <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ml-3">Beginning</span></h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2013-2018</time>
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">SD Muhammadiyah 1 Bandung</p>
+    </li>
+    <li class="mb-10 ml-6">
+        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </span>
+        <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">SEKOLAH MENENGAH PERTAMA</h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2018-2021</time>
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">SMP Muhammadiyah 4, Bandung</p>
+    </li>
+    <li class="ml-6">
+        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </span>
+        <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">SEKOLAH MENANGAH KEJURUAN</h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2021-2024</time>
+        <p class="text-base font-normal text-gray-500 dark:text-gray-400">SMK Assalaam, Bandung</p>
+    </li>
+</ol>
+</div>
+
+    <p class="text-4xl font-extrabold text-gray-900 dark:text-white"><center>Kemampuan</center>
+    </p>
+
+<div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-3" >
+<div class="flex items-center mb-5">
+    <p class="bg-blue-100 text-blue-800 text-sm font-semibold inline-flex items-center p-1.5 rounded dark:bg-blue-200 dark:text-blue-800">RPL</p>
+    <span class="w-1 h-1 mx-2 bg-gray-900 rounded-full dark:bg-gray-500"></span>
+    <p class="text-sm font-medium text-gray-500 dark:text-gray-400"></p>
+</div>
+<div class="gap-8 sm:grid sm:grid-cols-2">
+    <div>
+        <dl>
+            <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">HTML</dt>
+            <dd class="flex items-center mb-3">
+                <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                    <div class="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style="width: 88%"></div>
+                </div>
+                <span class="text-sm font-medium text-gray-500 dark:text-gray-400">8.8</span>
+            </dd>
+        </dl>
+        <dl>
+            <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">PHP</dt>
+            <dd class="flex items-center mb-3">
+                <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                    <div class="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style="width: 60%"></div>
+                </div>
+                <span class="text-sm font-medium text-gray-500 dark:text-gray-400">6.0</span>
+            </dd>
+        </dl>
+        <dl>
+            <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">CSS</dt>
+            <dd class="flex items-center mb-3">
+                <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                    <div class="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style="width: 42%"></div>
+                </div>
+                <span class="text-sm font-medium text-gray-500 dark:text-gray-400">4.2</span>
+            </dd>
+        </dl>
+        <dl>
+            <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">JS</dt>
+            <dd class="flex items-center">
+                <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                    <div class="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style="width: 54%"></div>
+                </div>
+                <span class="text-sm font-medium text-gray-500 dark:text-gray-400">5.4</span>
+            </dd>
+        </dl>
+    </div>
+    <div>
+        <dl>
+            <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">VUE.JS</dt>
+            <dd class="flex items-center mb-3">
+                <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                    <div class="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style="width: 52%"></div>
+                </div>
+                <span class="text-sm font-medium text-gray-500 dark:text-gray-400">5.2</span>
+            </dd>
+        </dl>
+    <div>
+        <dl>
+            <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">NODE.JS</dt>
+            <dd class="flex items-center mb-3">
+                <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                    <div class="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style="width: 60%"></div>
+                </div>
+                <span class="text-sm font-medium text-gray-500 dark:text-gray-400">6.0</span>
+            </dd>
+        </dl>
+        <div>
+        <dl>
+            <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">EXPRESS.JS</dt>
+            <dd class="flex items-center mb-3">
+                <div class="w-full bg-gray-200 rounded h-2.5 dark:bg-gray-700 mr-2">
+                    <div class="bg-blue-600 h-2.5 rounded dark:bg-blue-500" style="width: 70%"></div>
+                </div>
+                <span class="text-sm font-medium text-gray-500 dark:text-gray-400">7.0</span>
+            </dd>
+        </dl>
+    </div>
+    </div>
+    </div>
+    </div>
+</div>
+
+<div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-20">
+    <p class="text-4xl font-extrabold text-gray-900 dark:text-white"><center>Pengalaman Organisasi</center></p>
+    
+</div>
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16" style="background-image: url('putih1.jpg');">
+
+    <div class="grid md:grid-cols-3 gap-4">
+    
+   
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <center>
+    <a href="#">
+        
+        <img class="rounded-t-lg " src="ipm.jpg"/>
+
+    </a></center>
+    <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ikatan Pelajar Muhammadiyah</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Periode 2019-2020</p>
+    </div>
+</div>
+
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    
+    <center>
+    <a href="#">
+        
+        <img class="rounded-t-lg " src="karta.jpg"  />
+
+    </a></center>
+    <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Karang Taruna</h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Periode 2019-2021</p>
+    </div>
+</div>
+
+<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    
+    <center>
+    <a href="#">
+        
+        <img class="rounded-t-lg " src="tpk.jpg"  />
+
+    </a></center>
+    <div class="p-5">
+        <a href="#">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Tapak Suci </h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Periode 2015-2017</p>
+    </div>
+</div>
+ 
+</div>
+</div>
+<center>
+    <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-8"></div>
+    <p class="text-xl italic font-bold leading-relaxed text-gray-900 dark:text-white">
+        "Jika tidak mau mengambil Resiko, kau tidak dapat menciptakan Masa Depan"</p>
+    <p>- Monkey D Luffy</p>
+
+</center>
+
+<div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-8"></div>
+</template>
